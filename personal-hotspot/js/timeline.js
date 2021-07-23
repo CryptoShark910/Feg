4c1531065b499a9601b9ae275c1cb32978c49a80
CryptoShark910 <87836180+CryptoShark910@users.noreply.github.com>
2021-07-23 18:15:46 -0400

    Update timeline.js

--- personal-hotspot/js/timeline.js
+++ personal-hotspot/js/timeline.js
@@ -1,6 +1,8 @@
 jQuery(document).ready(function($){
     var timelines = $('.cd-horizontal-timeline'),
-        eventsMinDistance = 65;
+//         eventsMinDistance = 65;
+            eventsMinDistance = $(window).width() > 500 ? 65 : 55;
+
 
 
     (timelines.length > 0) && initTimeline(timelines);
